7618f08bf2ea981b9515548b951e297fca7f3dc4
Nathan A <fRenzyA05@gmail.com>
2023-09-13 14:23:16 -0300

    Added script to push objects away from a certain distance

--- Assets/PackBehaviour.cs
+++ Assets/PackBehaviour.cs
@@ -5,22 +5,52 @@ using UnityEngine;
 public class PackBehaviour : MonoBehaviour
 {
     public Rigidbody rb;
+    public Transform puller;
     public float rb_t = 10f;
+    public bool inSight;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddForce(0,100,10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        inSight = Physics.CheckSphere(transform.position, rb_t);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pull"))
+        {
+            puller = other.transform;
+        }
         
+        //puller = GameObject.FindWithTag("Pull").transform;
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Push"))
+        {
+            rb.AddForce(rb_t,rb_t,rb_t);
+        }
     }
     private void FixedUpdate()
     {
-        rb.AddForce(transform.up * rb_t);
+        if (inSight) 
+        {
+                  
+            rb.AddForce(puller.position - rb.transform.position,ForceMode.Force);
+        }       
     }
+
 
     private void OnDrawGizmosSelected()
     {
