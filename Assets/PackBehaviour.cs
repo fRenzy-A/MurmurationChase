78465e020c9842dfe78448dec39009aeff0eeca2
Nathan A <fRenzyA05@gmail.com>
2023-09-13 09:57:30 -0300

    Added a script and an object

--- Assets/PackBehaviour.cs
+++ Assets/PackBehaviour.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PackBehaviour : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float rb_t = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.up * rb_t);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
     }
 }
