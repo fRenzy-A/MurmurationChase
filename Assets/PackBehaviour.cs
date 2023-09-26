a3bbbf9a159585aa31b25489e284a29010c22c0e
Nathan A <fRenzyA05@gmail.com>
2023-09-25 22:35:39 -0300

    adding direction multipliers for pull

--- Assets/PackBehaviour.cs
+++ Assets/PackBehaviour.cs
@@ -8,13 +8,17 @@ public class PackBehaviour : MonoBehaviour
     public Transform puller;
     public bool inSight;
 
-    public float myVelocity;
+    public float maxVelocity;
 
 
     public float mySpeed;
-    public float x;
-    public float y;
-    public float z;
+
+    public float pullMultiplier;
+    public float pushAwayMultiplier;
+
+    public float pullNPushXMult;
+    public float pullNPushYMult;
+    public float pullNPushZMult;
 
     public float xBounds;
     public float yBounds;
@@ -24,7 +28,7 @@ public class PackBehaviour : MonoBehaviour
     public float forceOfBounds;
 
     public float pullRange = 10f;
-    public float pushRange = 3f;
+    public float pushRange = 1f;
     public bool outOfBounds;
 
     public float howMuchForce;
@@ -45,10 +49,13 @@ public class PackBehaviour : MonoBehaviour
         bounds = GameObject.FindWithTag("Push").GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
         outOfBounds = false;
-        rb.AddForce(Random.Range(5,10),Random.Range(1, 10), Random.Range(1, 10), ForceMode.Impulse);
-        xBounds = 20 + bounds.transform.position.x;
-        yBounds = 20 + bounds.transform.position.y;
-        zBounds = 20 + bounds.transform.position.z;
+
+        rb.AddForce(Random.Range(1,10),Random.Range(1, 10), Random.Range(1, 10), ForceMode.Impulse);
+
+
+        xBounds = (bounds.transform.localScale.x / 2) + bounds.transform.position.x;
+        yBounds = (bounds.transform.localScale.y / 2) + bounds.transform.position.y;
+        zBounds = (bounds.transform.localScale.z / 2) + bounds.transform.position.z;
         
         
     }
@@ -63,29 +70,32 @@ public class PackBehaviour : MonoBehaviour
     
     private void FixedUpdate()
     {
-        x = rb.velocity.x; y = rb.velocity.y; z = rb.velocity.z;
+        //x = rb.velocity.x; y = rb.velocity.y; z = rb.velocity.z;
         mySpeed = rb.velocity.magnitude;
         //cap the maximum velocity/speed
-        if ((rb.velocity.magnitude > myVelocity && rb.velocity.magnitude > 0) || (rb.velocity.magnitude > -myVelocity && rb.velocity.magnitude < 0))
+        if ((rb.velocity.magnitude > maxVelocity && rb.velocity.magnitude > 0) || (rb.velocity.magnitude > -maxVelocity && rb.velocity.magnitude < 0))
         {
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, myVelocity);
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
         }
         Collider[] inMyRadius = Physics.OverlapSphere(rb.transform.position,pullRange);
 
         Collider[] inMyPushRadius = Physics.OverlapSphere(rb.transform.position, pushRange);
         howMany = inMyPushRadius;
-        
-        foreach (Collider c in inMyRadius) 
+
+        foreach (Collider c in inMyRadius)
         {
             if (c.CompareTag("Push"))
             {
                 return;
             }
-            
-            else if (c.transform.root != c.transform)
+            /*else if (c.transform.root != c.transform)
             {
-                rb.AddForce(c.transform.position - rb.transform.position, ForceMode.Acceleration);
-            }        
+                
+            }*/
+            else
+            {
+                rb.AddForce((c.transform.position - rb.transform.position), ForceMode.Force);
+            }
         }
         foreach (Collider d in inMyPushRadius)
         {
@@ -93,11 +103,14 @@ public class PackBehaviour : MonoBehaviour
             {
                 return;
             }
-            else if (d.transform.root != d.transform)
+            else if (d.transform != d.transform.root)
             {
-               
-                rb.AddForce(d.transform.position + rb.transform.position * 3, ForceMode.Acceleration);
+                Vector3 dir = d.transform.position - transform.position;
+                dir = -dir;
+                rb.AddForce(dir * 5, ForceMode.Impulse);
             }
+            
+            //else rb.AddForce(d.transform.position + rb.transform.position, ForceMode.Impulse);
         }
         Bounds();
     }
@@ -108,83 +121,35 @@ public class PackBehaviour : MonoBehaviour
         if ((xBounds < rb.transform.position.x) && rb.transform.position.x > bounds.transform.position.x)
         {
             //xOOB = xOOB * -1;
-            rb.AddForce(-xOOB * 2, 0, 0);
+            rb.AddForce(-xOOB * 2, 0, 0,ForceMode.Acceleration);
         }
         else if ((-xBounds > rb.transform.position.x) && rb.transform.position.x < bounds.transform.position.x)
         {
             //xOOB = xOOB * -1;
-            rb.AddForce(xOOB * 2, 0, 0);
+            rb.AddForce(xOOB * 2, 0, 0, ForceMode.Acceleration);
         }
 
         if ((yBounds < rb.transform.position.y) && rb.transform.position.y > bounds.transform.position.y)
         {
             //yOOB = yOOB * -1;
-            rb.AddForce(0, -yOOB * 2, 0);
+            rb.AddForce(0, -yOOB * 2, 0, ForceMode.Acceleration);
         }
         else if ((-yBounds > rb.transform.position.y) && rb.transform.position.y < bounds.transform.position.y)
         {
-            rb.AddForce(0, yOOB * 2, 0);
+            rb.AddForce(0, yOOB * 2, 0, ForceMode.Acceleration);
         }
 
         if ((zBounds < rb.transform.position.z) && rb.transform.position.z > bounds.transform.position.z)
         {
             //zOOB = zOOB * -1;
-            rb.AddForce(0, 0, -zOOB * 2);
+            rb.AddForce(0, 0, -zOOB * 2, ForceMode.Acceleration);
         }
         else if ((-zBounds > rb.transform.position.z) && rb.transform.position.z < bounds.transform.position.z)
         {
-            rb.AddForce(0, 0, zOOB * 2);
+            rb.AddForce(0, 0, zOOB * 2, ForceMode.Acceleration);
         }
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        /*if (other.gameObject.CompareTag("Push"))
-        {
-            x *= -x;
-            outOfBounds = true
-        }*/
 
-        outOfBounds = false;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        /*if (other.gameObject.CompareTag("Push"))
-        {
-            if (((bounds.transform.localScale.x < rb.transform.position.x) && rb.transform.position.x > 0) || ((-bounds.transform.localScale.x > rb.transform.position.x) && rb.transform.position.x < 0))
-            {
-                x = rb.velocity.x * -1;
-            }
-            if (((bounds.transform.localScale.y < rb.transform.position.y) && rb.transform.position.y > 0) || ((-bounds.transform.localScale.y > rb.transform.position.y) && rb.transform.position.y < 0))
-            {
-                y = rb.velocity.y * -1;
-            }
-            if (((bounds.transform.localScale.z < rb.transform.position.z) && rb.transform.position.z > 0) || ((-bounds.transform.localScale.z > rb.transform.position.z) && rb.transform.position.z < 0))
-            {
-                z = rb.velocity.z * -1;
-            }
-            
-        }*/
-        /*if (other.gameObject.CompareTag("Push"))
-        {
-            if (((xBounds < rb.transform.position.x) && rb.transform.position.x > 0) || ((-xBounds > rb.transform.position.x) && rb.transform.position.x < 0))
-            {
-                xOOB = xOOB * -1;
-                
-            }
-            if (((yBounds < rb.transform.position.y) && rb.transform.position.y > 0) || ((-yBounds > rb.transform.position.y) && rb.transform.position.y < 0))
-            {
-                yOOB = yOOB * -1;
-                
-            }
-            if (((zBounds < rb.transform.position.z) && rb.transform.position.z > 0) || ((-zBounds > rb.transform.position.z) && rb.transform.position.z < 0))
-            {
-                zOOB = zOOB * -1;
-                
-            }
-        }*/
-        outOfBounds = true;
-
-    }
 
     private void OnDrawGizmosSelected()
     {
